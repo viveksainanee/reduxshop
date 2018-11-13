8f5ca90e63dcb2cfbad8f977eabd8de518007d81
Vivek Sainanee <viveksainanee@gmail.com>
2018-11-13 14:17:35 -0800

    initial commit

---
+++ src/ItemList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import data from './db.json'
+
+
+class ItemList extends Component {
+  render() {
+    return (
+      //Item Component
+      
+    );
+  }
+}
+
+export default ItemList;
